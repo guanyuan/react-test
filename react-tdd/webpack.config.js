90e128e1aa2f5d58a2091ca528f5433ed19ee8ce
guanyuan <yuanguan89@gmail.com>
2016-10-19 23:33:29 +0800

    config webpack

--- react-tdd/webpack.config.js
+++ react-tdd/webpack.config.js
@@ -1,7 +1,5 @@
-// our webpack.config.js file located in project root
-
 var webpack = require('webpack');
-var path = require('path');                // a useful node path helper library
+var path = require('path');
 
 var config = {
   entry: [
@@ -9,21 +7,32 @@ var config = {
     'webpack-dev-server/client?http://localhost:3000',
     './src/main.js'
   ],
+  resolve: {
+    root: [
+      // allows us to import modules as if /src was the root.
+      // so I can do: import Comment from 'components/Comment'
+      // instead of:  import Comment from '../components/Comment' or whatever relative path would be
+      path.resolve(__dirname, './src')
+    ],
+    // allows you to require without the .js at end of filenames
+    // import Component from 'component' vs. import Component from 'component.js'
+    extensions: ['', '.js', '.json', '.jsx']
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'), // store the bundled output in dist/bundle.js
-    filename: 'bundle.js'                  // specifying file name for our compiled assets
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        loaders: ['babel'],
-        exclude: /node_modules/,
-        query: {
-          presets: ['react', 'es2015']
-        }
+        test: /\.js?$/,
+        // dont run node_modules or bower_components through babel loader
+        exclude: /(node_modules|bower_components)/,
+        // babel is alias for babel-loader
+        // npm i babel-core babel-loader --save-dev
+        loader: 'babel'
       }
-    ]
+    ],
   }
 }
 
